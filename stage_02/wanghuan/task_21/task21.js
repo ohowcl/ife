87ec993a336cc2a7b16c1db2403acb741b622bc4
wang huan <15611002722@163.com>
2016-04-08 16:23:11 +0800

    添加丑丑的21，实验室活干完再用原型模型重构

--- stage_02/wanghuan/task_21/task21.js
+++ stage_02/wanghuan/task_21/task21.js
@@ -1,16 +1,20 @@
 var btn1 = document.getElementById('addbutton1');
-var btn2 = document.getElementById('addbutton2');
-var btn3 = document.getElementById('delbutton1');
-var btn4 = document.getElementById('delbutton2');
-var btn5 = document.getElementById('selbutton');
+var div1 = document.getElementById('divShow');
 var array = [];
+var arrayTag = [];
 var pattern = /\s+/;
 
-function changeDiv() {
-  var len = document.getElementById('divShow').children.length;
+function changeDiv(divshow) {
+  //删除之前在页面上显示的div
+  var len = divshow.children.length;
   for(var j = 0;j < len;j++) {
-    document.getElementById('divShow').removeChild(document.getElementById('divShow').children[0]);
+    divshow.removeChild(divshow.children[0]);
   }
+  var len2 = array.length;
+  if(array.length > 10){
+    array = array.slice(len2-10);
+  }
+  //添加数组对应的所有div于页面上
   for(var i = 0;i < array.length;i++) {
     var divEvery = document.createElement("div");
     divEvery.innerHTML = array[i];
@@ -19,15 +23,61 @@ function changeDiv() {
   }
 }
 
-function addLeft() {
-  var textValue = document.getElementById('textvalue').value.trim();
+function changeDiv2(divshow) {
+  //删除之前在页面上显示的div
+  var len = divshow.children.length;
+  for(var j = 0;j < len;j++) {
+    divshow.removeChild(divshow.children[0]);
+  }
+  var len2 = arrayTag.length;
+  if(arrayTag.length > 10){
+    arrayTag = arrayTag.slice(len2-10);
+  }
+  //添加数组对应的所有div于页面上
+  for(var i = 0;i < arrayTag.length;i++) {
+    var divEvery = document.createElement("div");
+    divEvery.innerHTML = arrayTag[i];
+    divEvery.setAttribute("class","div");
+    document.getElementById('divShow1').appendChild(divEvery);
+  }
+}
 
-  if((document.getElementById('textvalue').value) == "") {
+function updateTag(e) {
+  var str = this.value;
+  var flag = true,
+      k = 0;
+  if(/(,| |\，)$/.test(str)||e.keyCode===13) {
+    var newTag = str.match(/(^[^,\， ]*)/)[0];
+    if(arrayTag.length === 0) {
+      arrayTag.push(newTag);
+    }else {
+      for(var k = 0;k < arrayTag.length;k++) {
+        //验证是否有重复值
+        if(arrayTag[k] === newTag) {
+          alert(arrayTag[k] + "exists");
+          flag = false;
+          break;
+        }
+        else {
+          flag = true;
+        }
+      }
+      if(flag === true) {
+        arrayTag.splice(k++,0,newTag);
+      }
+    }
+  }
+  changeDiv2(document.getElementById('divShow1'));
+}
+
+function addLeft(text) {
+  var textValue = text.value.trim();
+  if((text.value) == "") {
       alert("input can not be null");
       return;
   }
   //clear the input value
-  document.getElementById('textvalue').value = "";
+  text.value = "";
   var array1 = textValue.split(",");
   var k = 0;
   var flag = true;
@@ -37,6 +87,7 @@ function addLeft() {
     var array2 = array1[i].split(/\s+/);
     for(var j = 0;j < array2.length;j++) {
       for(var k = 0;k < array.length;k++) {
+        //验证是否有重复值
         if(array[k] === array2[j]) {
           alert(array[k] + "exists");
           flag = false;
@@ -52,7 +103,7 @@ function addLeft() {
     }
   }
 
-  changeDiv();
+  changeDiv(document.getElementById('divShow'));
 }
 
 function delDivHandle(target) {
@@ -66,14 +117,32 @@ function delDivHandle(target) {
 }
 
 
-function init() {
-  btn1.onclick = addLeft;
+function delDivHandle(target) {
+ var content = target.innerHTML;
+ for(var i = 0;i < array.length;i++) {
+   if(content === array[i]) {
+     array.splice(i,1);
+   }
+ }
+ changeDiv();
+}
 
-  var div1 = document.getElementById('divShow');
+function init() {
+  // add interests btn event
+  btn1.addEventListener("click",function(event) {
+
+    if(event.target && event.target.previousSibling.nodeName === "#text") {
+      addLeft(document.getElementById('textvalue'));
+    }
+  })
+
   div1.addEventListener("click", function(event) {
 		if (event.target && event.target.nodeName === "DIV") {
  			delDivHandle(event.target);
  		}
  	})
+
+  var tagT = document.getElementById('tagText');
+  tagT.onkeyup = updateTag;
 }
 init();
